7e2fdab55f7d30892a1d92ab6403227bef523f47
Pravil Wijesinghe <111410309+Pravil-Wijesinghe@users.noreply.github.com>
2025-05-17 22:09:31 +0530

    feat: update Ads component with new content and layout adjustments for better engagement

--- frontend/src/components/home/Ads.jsx
+++ frontend/src/components/home/Ads.jsx
@@ -7,16 +7,26 @@ function Ads() {
         sx={{
             mx: 28,
             backgroundColor: 'primary.main',
-            height: 200,
             color: 'white',
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
-            alignItems: 'center',
+            alignItems: 'left',
+            padding: 6,
             my: 10,
+            gap: 2,
+            borderRadius: 1,
         }}
     >
-     <Typography variant="h2" color='white' sx={{ fontWeight: 'bold', color: 'white' }}>
-        Lorem ipsum dolor sit amet, consectetur. 
+     <Typography variant="h3" color='white' sx={{ fontWeight: 'bold', color: 'white' }}>
+        Stay Healthy with Med Connect! 
+      </Typography>
+      <Typography variant="subtitle" color='white' sx={{ fontWeight: 'medium', color: 'white' }}>
+        Experience seamless healthcare solutions tailored just for you. Whether you're looking for expert medical advice, scheduling appointments, or accessing cutting-edge medical 
+        resources, <b>Med Connect</b> is your trusted partner in health.
+      </Typography>
+      <Typography variant="subtitle" color='white' sx={{ fontWeight: 'medium', color: 'white' }}>
+        Join us today and take the first step towards a healthier tomorrow. Your well-being is our priority!
       </Typography>
     </Box>
   )
